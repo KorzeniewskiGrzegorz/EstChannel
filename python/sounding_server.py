41e2194d2be348216a5b57ede3253e0b0dc8f3a4
Grzegorz <korzeniewski.gregor@gmail.com>
2019-07-01 17:43:03 -0500

    server version of sounding

--- python/sounding_server.py
+++ python/sounding_server.py
@@ -1,18 +1,33 @@
 # TX, only task is to wait for a signal to start to transmitt and then send back ruido data
 import socket
+import paramiko
+import os
+import sys
 
 import threading
 from whiteNoiseGen import whiteNoiseGen
 from sondeoWithoutGui import *
 from dataProcess import dataProcess
-from sondeoRx import *
+from sondeoTx import *
 
-HOST = '192.168.10.3'                 # Symbolic name meaning all available interfaces
+
+
+
+
+ip = '192.168.10.4'
+#ip = '192.168.10.3'
+name = "udg"
+#name = "grzechu"
+
+HOST ='192.168.10.3'              # Symbolic name meaning all available interfaces
 #HOST = '127.0.0.1'   
-PORT = 50008              # Arbitrary non-privileged port
+PORT = 50007             # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
+
+
+
 conn, addr = s.accept()
 print 'Connected by', addr
 
@@ -24,10 +39,6 @@ offman = 15;
 wd = 10; # window duration [us] for the correlation purpose
 
 
-#whiteNoiseGen(Fs)
-#print("###############################")
-#print("\n\n\n\n\n\n\n")
-
 message = "" # take input
 shut = False
 pozycja = 0
@@ -35,19 +46,26 @@ pozycja = 0
 
 while message.lower().strip() != 'narazie' and not shut:
 
-	data = s.recv(1024).decode()  # receive response
+	data = conn.recv(1024).decode()  # receive response
 
-	print('Received from client: ' + data)  # show in terminal
 	if data == "start":
 		
-	sondeoTx()
-
-
-	else:
-		print("nie rozumiem, rozlaczam sie")
-		shut = True
+        	sondeoTx()
+        	message = "done" 
+        	conn.send(message.encode())
+        	os.system("scp -i ~/.ssh/id_rsa.pub /dev/shm/ruido* "+name +"@"+ip+":/dev/shm/")
+        	print("udalo sie")
+        	shut=True
 
 
 
 
 conn.close()  # close the connection
+
+#try: 
+#	while True:
+	#	print("co jest stefan")
+#		conn.close()
+	#	sys.exit(0)
+#except KeyboardInterrupt:
+#	print("mam cie")

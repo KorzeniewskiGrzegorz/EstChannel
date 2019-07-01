2b9b26f794bae868fd9afbdd4aadf53ff14dbfbd
udg <korzeniewski.gregor@gmail.com>
2019-07-01 14:59:20 -0500

    reversed tasks of server and client

--- python/sounding_client.py
+++ python/sounding_client.py
@@ -1,33 +1,28 @@
-# Echo client program
+# RX, where the data is generated and processed
 import socket
 
-HOST = '192.168.10.4'         # The remote host
+HOST = '192.168.10.3'         # The remote host
 #HOST = '127.0.0.1'   
 PORT = 50008              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conn.connect((HOST, PORT))
 
 
 
 
-
-message = "" # take input
+response =""
+message = "start"  # 
+conn.send(message.encode())  # send message
 shut = False
-pozycja = 0
 
+sondeoRx()
 
-while message.lower().strip() != 'narazie' and not shut:
+while message.lower().strip() != 'zamknij' and not shut:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+	response = conn.recv(1024).decode()
+	print("from connected user: " + str(response))
 
-	data = s.recv(1024).decode()  # receive response
-
-	print('Received from server: ' + data)  # show in terminal
-	if data == "start":
-		
-	sondeoTx()
-
-
-	else:
-		print("nie rozumiem, rozlaczam sie")
+	if response == "done":
 		shut = True
 
 
@@ -35,4 +30,4 @@ while message.lower().strip() != 'narazie' and not shut:
 
 	 
 
-s.close()  # close the connection
+conn.close()  # close the connection
--- python/sounding_server.py
+++ python/sounding_server.py
@@ -1,4 +1,4 @@
-# Echo server program
+# TX, only task is to wait for a signal to start to transmitt and then send back ruido data
 import socket
 
 import threading
@@ -7,7 +7,7 @@ from sondeoWithoutGui import *
 from dataProcess import dataProcess
 from sondeoRx import *
 
-HOST = '192.168.10.4'                 # Symbolic name meaning all available interfaces
+HOST = '192.168.10.3'                 # Symbolic name meaning all available interfaces
 #HOST = '127.0.0.1'   
 PORT = 50008              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,22 +28,25 @@ wd = 10; # window duration [us] for the correlation purpose
 #print("###############################")
 #print("\n\n\n\n\n\n\n")
 
-
-
-response =""
-message = "start"  # 
-conn.send(message.encode())  # send message
+message = "" # take input
 shut = False
+pozycja = 0
 
-sondeoRx()
 
-while message.lower().strip() != 'zamknij' and not shut:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-	response = conn.recv(1024).decode()
-	print("from connected user: " + str(response))
+while message.lower().strip() != 'narazie' and not shut:
 
-	if response == "done":
+	data = s.recv(1024).decode()  # receive response
+
+	print('Received from client: ' + data)  # show in terminal
+	if data == "start":
+		
+	sondeoTx()
+
+
+	else:
+		print("nie rozumiem, rozlaczam sie")
 		shut = True
+
 
 
 

4e7a66a4c65b797fb256a7a46f9e8027d32e044a
udg <korzeniewski.gregor@gmail.com>
2020-02-28 17:36:41 -0600

    new version of spatial simulation, ed quimica analisis

--- analisis/analisis_spatial_LosVsNlos.m
+++ analisis/analisis_spatial_LosVsNlos.m
@@ -52,3 +52,124 @@ for j=1:measurePoints
 end
 
 
+%%% LoS spatial avg
+
+a_pdpLos(1,:) = pdpx(1,:);
+a_pdpLos(2,:) = pdpx(3,:);
+
+sPDPLoS = mean(a_pdpLos);
+
+figure
+stem(t,sPDPLoS,'black')
+title("sPDP LoS")
+xlabel('Retardo [ns]'), ylabel('Magnitud norm. [u.a.]') 
+axis([0 700 0 1])
+grid on
+
+[tmeanLos,trmsLos,tmaxLos,b_50Los]=paramDelay(t, sPDPLoS,plotMode,noiseThr);
+
+%%% NLoS spatial avg
+
+a_pdpNLos(1,:) = pdpx(2,:);
+a_pdpNLos(2,:) = pdpx(4,:);
+a_pdpNLos(3,:) = pdpx(5,:);
+
+sPDPNLoS = mean(a_pdpNLos);
+
+figure
+stem(t,sPDPNLoS,'black')
+title("sPDP NLoS")
+xlabel('Retardo [ns]'), ylabel('Magnitud norm. [u.a.]') 
+axis([0 700 0 1])
+grid on
+
+[tmeanNLos,trmsNLos,tmaxNLos,b_50NLos]=paramDelay(t, sPDPNLoS,plotMode,noiseThr);
+
+
+%%%%%%%% Simulations
+
+%%% LoS
+
+clusterFirstIdx = [6  9 13 15];
+clusterLastIdx  = [8 12 14 17];
+type = 0; %% 0 for LoS
+[ tSimLos ,pdpSimLos, LambdaLos,lambdaLos,GammaLos,gammaLos,gam0Los,aLos] =  SalehValCalcFunc(sPDPLoS,Fs,clusterFirstIdx,clusterLastIdx,type,noiseThr);
+
+tSimLos = mean(tSimLos);
+pdpSimLos=mean(pdpSimLos);
+pdpSimLos = pdpSimLos/max(pdpSimLos);
+
+[tmeanSimLos,trmsSimLos,tmaxSimLos,b_50SimLos]=paramDelay(tSimLos, pdpSimLos,plotMode,noiseThr);
+
+figure
+stem(t,sPDPLoS,'black')
+
+title("sPDP LoS Estimado vs Simulado")
+xlabel('Retardo [ns]'), ylabel('Magnitud norm. [u.a.]') 
+grid on
+hold on
+
+%%% alignment of plots to its maxes
+[v, dm] = max(sPDPLoS);
+[v, dsim] = max(pdpSimLos);
+
+offsetM = (dm-1)/Fs*1e9 ; % ns
+offsetS = tSimLos(dsim);
+d = abs(dm-dsim);
+
+offset = abs(offsetM-offsetS);
+%%%
+
+plot(tSimLos(1:100) + offset,pdpSimLos(1:100),'r--')
+axis([0 700 0 1])
+
+leg{1} = "Estimado";
+leg{2} = "Simulado";
+legend(leg)
+
+hold off
+
+
+%%% NLoS
+
+clusterFirstIdx = [6 7 10 13];
+clusterLastIdx  = [7 9 12 16];
+type = 1; %% 0 for LoS
+[ tSimNLos ,pdpSimNLos, LambdaNLos,lambdaNLos,GammaNLos,gammaNLos,gam0NLos,aNLos] =  SalehValCalcFunc(sPDPNLoS,Fs,clusterFirstIdx,clusterLastIdx,type,noiseThr);
+
+tSimNLos = nanmean(tSimNLos);
+pdpSimNLos = nanmean(pdpSimNLos);
+
+pdpSimNLos = pdpSimNLos/max(pdpSimNLos);
+
+[tmeanSimNLos,trmsSimNLos,tmaxSimNLos,b_50SimNLos]=paramDelay(tSimNLos, pdpSimNLos,plotMode,noiseThr);
+
+figure
+stem(t,sPDPNLoS,'black')
+
+title("sPDP NLoS Estimado vs Simulado")
+xlabel('Retardo [ns]'), ylabel('Magnitud norm. [u.a.]') 
+grid on
+hold on
+
+%%% alignment of plots to its maxes
+[v, dm] = max(sPDPNLoS);
+[v, dsim] = max(pdpSimNLos);
+
+offsetM = (dm-1)/Fs*1e9 ; % ns
+offsetS = tSimNLos(dsim);
+d = abs(dm-dsim);
+
+offset = abs(offsetM-offsetS);
+%%%
+
+plot(tSimNLos(1:100) + offset,pdpSimNLos(1:100),'r--')
+axis([0 700 0 1])
+
+leg{1} = "Estimado";
+leg{2} = "Simulado";
+legend(leg)
+
+hold off
+
+
